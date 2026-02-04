8b3a0b5625e6e5b0ca29e3da783479678e6f4723
jaronlong52 <longjm3@appstate.edu>
2026-02-04 11:22:44 -0500

    Updated project name everywhere to UrlPulse. Added UI and functionality for main page

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace UrlPulse.Pages;
 
 public class IndexModel : PageModel
 {
     [BindProperty]
-    public string? InputValue { get; set; }
+    [Required(ErrorMessage = "Please enter a valid URL")]
+    [Url(ErrorMessage = "Invalid URL format")]
+    public string InputValue { get; set; }
 
     public void OnGet()
     {
-        // Runs when the page is loaded
+        // This is where you will eventually load the list of URLs from the database
     }
 
-    public void OnPost()
+    public IActionResult OnPost()
     {
-        // Runs when the form is submitted
-        Console.WriteLine($"Form submitted with value: {InputValue}");
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        // TODO: Logic to save the URL to the database
+
+        return RedirectToPage(); // Refresh page to see new entry
     }
 }
--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MyWebApp.Pages;
+namespace UrlPulse.Pages;
 
 public class PrivacyModel : PageModel
 {
