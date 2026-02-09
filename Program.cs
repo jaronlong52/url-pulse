540b28027c56c0992c438eabef0230fe1aaffdec
jaronlong52 <longjm3@appstate.edu>
2026-02-09 16:42:09 -0500

    Simplified docker configuration, removed docker compose

--- Program.cs
+++ Program.cs
@@ -4,35 +4,28 @@ using UrlPulse.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
-// Add DbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add HttpClient for making HTTP requests
 builder.Services.AddHttpClient();
 builder.Services.AddHttpClient<IUrlChecker, UrlChecker>();
 
 var app = builder.Build();
 
-// APPLY EF CORE MIGRATIONS
+// Apply migrations (acceptable for single-container apps)
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     db.Database.Migrate();
 }
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
     app.UseHsts();
-}
-else
-{
-    app.UseHttpsRedirection();
 }
 
 app.UseHttpsRedirection();
