5e5aa11cebd046f6a791f2702d8ddabcc2940386
jaronlong52 <longjm3@appstate.edu>
2026-02-28 14:40:47 -0500

    added IsPaused to UrlMonitor model and created database migration

--- Migrations/20260228193808_AddIsPausedToUrlMonitor.Designer.cs
+++ Migrations/20260228193808_AddIsPausedToUrlMonitor.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UrlPulse.Data;
@@ -11,9 +12,11 @@ using UrlPulse.Data;
 namespace UrlPulse.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260228193808_AddIsPausedToUrlMonitor")]
+    partial class AddIsPausedToUrlMonitor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20260228193808_AddIsPausedToUrlMonitor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlPulse.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsPausedToUrlMonitor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaused",
+                table: "UrlMonitors",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPaused",
+                table: "UrlMonitors");
+        }
+    }
+}
