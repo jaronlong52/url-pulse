e9008d87427dce9e1a046c7b23d7c30f18617a69
jaronlong52 <longjm3@appstate.edu>
2026-04-08 15:09:04 -0400

    Added Region to LatencyHistories model, added steps in deploy.yml to handle to azure functions, and created a database migration

--- UrlPulse.Core/Models/LatencyHistory.cs
+++ UrlPulse.Core/Models/LatencyHistory.cs
@@ -7,6 +7,7 @@ public class LatencyHistory
   public int StatusCode { get; set; }
   public string ErrorMessage { get; set; } = string.Empty;
   public DateTime CheckedAt { get; set; } = DateTime.UtcNow; // UTC time
+  public string Region { get; set; } = string.Empty;
   public int UrlMonitorId { get; set; } // Foreign Key
   public UrlMonitor UrlMonitor { get; set; } = null!; // Navigation Property: Link back to the parent
 }
